0e0c34c6e5042092597c2cedd2b8f75e159557d2
Iggy-dev <71981882+Iggy-dev@users.noreply.github.com>
2024-10-29 10:35:39 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById("buy-button").addEventListener("click", function () {
+    alert("¡Gracias por tu interés! Te redirigiremos a la página de compra.");
+    // Aquí podrías redirigir a otra página o añadir al carrito, etc.
+});
